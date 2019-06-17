7c10cea0d47c485c1520546b69e41466e990049b
LBWright <lwright.js@gmail.com>
2019-06-17 16:44:57 -0500

    finishes clients routes

--- app/api/clients.py
+++ app/api/clients.py
@@ -22,16 +22,34 @@ def create_client():
     return jsonify(data=client_schema.dump(client))
 
 
-@bp.route("/clients/<int:id>", methods=["GET"])
-def get_single_client():
-    pass
+@bp.route("/clients/<int:client_id>", methods=["GET"])
+def get_single_client(client_id):
+    client = Client.query.get(client_id)
+
+    if not client:
+        return abort(404, "Client not found")
+
+    return jsonify(data=client_schema.dump(client))
 
 
-@bp.route("/clients/<int:id>", methods=["PUT"])
-def update_client():
-    pass
+@bp.route("/clients/<int:client_id>", methods=["PUT"])
+def update_client(client_id):
+    client = Client.query.get(client_id)
+    if not client:
+        return abort(404, "Client not found")
+
+    new_client = client_schema.load(request.get_json())
+    client.update(new_client)
+
+    return jsonify(data=client_schema.dump(new_client))
 
 
-@bp.route("/clients/<int:id>", methods=["DELETE"])
-def delete_client():
-    pass
+@bp.route("/clients/<int:client_id>", methods=["DELETE"])
+def delete_client(client_id):
+    client = Client.query.get(client_id)
+    if not client:
+        return abort(404, "Client not found")
+
+    client.delete()
+
+    return jsonify(data=client_schema.dump(client)), 202
